1b0abfba36c0b371291dd88d08e22fa805c4c67f
Prasanna-chowdary <sriprasanna2496@gmail.com>
2026-03-25 13:27:39 +0530

    Add files via upload

---
+++ functionparameter.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+var result = add(10, 20);
+console.log("Addition:", result);
+function greet(name) {
+    return "Hello " + name;
+}
+console.log(greet("Pranathi"));
+function checkAge(age) {
+    return age >= 18;
+}
+console.log("Is Adult:", checkAge(18));
